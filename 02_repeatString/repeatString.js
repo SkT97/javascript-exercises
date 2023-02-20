7a18d2175caa7877ec6fd5500a11c736886256ff
SkT97 <s.velkov.97@gmail.com>
2023-02-20 16:36:07 +0100

    foundations-fundamentals-part-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+	let retString = "";
+
+	if (num < 0) {
+		retString = "ERROR"
+	}
+	else {
+		for (let i = 0; i < num; i++) {
+			retString += string;
+		}
+	}
+	return retString;
 };
 
 // Do not edit below this line
